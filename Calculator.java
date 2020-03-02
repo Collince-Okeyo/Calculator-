56697ddbc357c727e7849acd9de417b8507ccba0
JoelKanyi <joelkanyi98@gmail.com>
2020-03-02 18:17:03 +0300

    A simple calculator in JAva

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+
+    public int addition(int a, int b){
+        return a+b;
+    }
+
+    public int multiplation(int a, int b){
+        return a*b;
+    }
+
+    public int division(int a, int b){
+        return a/b;
+    }
+    public int modulus(int a, int b){
+        return a%b;
+    }
+
+
+
+    public static void main(String[]args){
+
+        Calculator calculator = new Calculator();
+
+    }
+}
